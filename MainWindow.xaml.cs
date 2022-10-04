7c35051b351fb495c431ec804a8f54d7410fd18c
elizavetacher44@gmail.com <elizavetacher44@gmail.com>
2022-10-04 13:24:43 +1000

    котики отображаются

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,7 +27,42 @@ namespace Wpf_Cherednichenko_PR5
 
         private void pict1_Click(object sender, RoutedEventArgs e)
         {
-            
+            img1.Source = new BitmapImage(new Uri("Pictures/kitty1.jpg", UriKind.Relative));
+        }
+
+        private void pict2_Click(object sender, RoutedEventArgs e)
+        {
+            img1.Source = new BitmapImage(new Uri("Pictures/kitty2.jpg", UriKind.Relative));
+        }
+
+        private void pict3_Click(object sender, RoutedEventArgs e)
+        {
+            img1.Source = new BitmapImage(new Uri("Pictures/kitty3.jpg", UriKind.Relative));
+        }
+
+        private void pict4_Click(object sender, RoutedEventArgs e)
+        {
+            img1.Source = new BitmapImage(new Uri("Pictures/kitty4.jpg", UriKind.Relative));
+        }
+
+        private void pict5_Click(object sender, RoutedEventArgs e)
+        {
+            img1.Source = new BitmapImage(new Uri("Pictures/kitty5.jpg", UriKind.Relative));
+        }
+
+        private void pict6_Click(object sender, RoutedEventArgs e)
+        {
+            img1.Source = new BitmapImage(new Uri("Pictures/kitty6.jpg", UriKind.Relative));
+        }
+
+        private void pict7_Click(object sender, RoutedEventArgs e)
+        {
+            img1.Source = new BitmapImage(new Uri("Pictures/kitty7.jpg", UriKind.Relative));
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
